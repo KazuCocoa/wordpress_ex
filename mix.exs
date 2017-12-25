99096fa0ccb7a5cc60c2066fed093a79591c3b88
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2017-12-25 12:37:13 +0900

    bump up supported elixir

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WordpressEx.Mixfile do
   def project() do
     [app: :wordpress_ex,
      version: "0.0.1",
-     elixir: "~> 1.1",
+     elixir: "~> 1.5",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
