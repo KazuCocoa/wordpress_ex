0e72b979b15cc7abf016e0e0b95e26d4ed14733d
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2020-12-24 01:19:21 -0800

    update

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WordpressEx.Mixfile do
   def project() do
     [app: :wordpress_ex,
      version: "0.0.2",
-     elixir: "~> 1.7",
+     elixir: "~> 1.11",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
